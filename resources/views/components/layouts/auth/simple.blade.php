50addbcaa7e647e0fa456a2f9718b1d6e53b6f30
S Rasheed <ksrasheed22@gmail.com>
2025-12-26 19:22:33 +0300

    style: update login page logo, and sign in & sign up pages background image

--- resources/views/components/layouts/auth/simple.blade.php
+++ resources/views/components/layouts/auth/simple.blade.php
@@ -3,14 +3,14 @@
     <head>
         @include('partials.head')
     </head>
-    <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
+    <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900 bg-cover bg-center" style="background-image: url('{{ asset('assets/img/featured/8.jpg') }}')">
         <div class="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-            <div class="flex w-full max-w-sm flex-col gap-2">
+            <div class="flex w-full max-w-sm flex-col gap-2 bg-white dark:bg-gray-800 rounded-xl shadow-xl p-8 w-full max-w-md">
                 <a href="{{ route('home') }}" class="flex flex-col items-center gap-2 font-medium" wire:navigate>
                     <span class="flex h-9 w-9 mb-1 items-center justify-center rounded-md">
-                        <x-app-logo-icon class="size-9 fill-current text-black dark:text-white" />
+                        <img src="{{ asset('assets/img/logo.png') }}" class="mr-3 h-8" alt="Shopper Simple Cart Logo" />
                     </span>
-                    <span class="sr-only">{{ config('app.name', 'Laravel') }}</span>
+                    <span class="sr-only">{{ config('app.name', 'Simple Shopper Cart') }}</span>
                 </a>
                 <div class="flex flex-col gap-6">
                     {{ $slot }}
