d023446df85395aa7c35415f23db61bea4b1c720
S Rasheed <ksrasheed22@gmail.com>
2025-12-26 15:16:34 +0300

    chore: update CartItem fillable to use user_id instead of session_id

--- app/Models/CartItem.php
+++ app/Models/CartItem.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class CartItem extends Model
 {
-    protected $fillable = ['product_id', 'session_id', 'quantity'];
+    protected $fillable = ['user_id', 'product_id', 'quantity'];
 
     public function product()
     {
